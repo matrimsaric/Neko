8431c3a3e65e33c784d34d1d858fba6e6bb0b55e
matrimsaric <matrimsaric@hotmail.co.uk>
2024-06-09 19:32:39 +0100

    test4

--- ServerCommonModule/Database/DbFactory.cs
+++ ServerCommonModule/Database/DbFactory.cs
@@ -19,6 +19,9 @@ namespace ServerCommonModule.Database
             DbUtilityParameter dbParams = new()
             {
                 ConnectionString = connectionString
+
+
+                
             };
             
             switch (connectionType)
